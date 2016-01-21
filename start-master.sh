071274cd7503ff204d2b7909d3246e7198365437
Guy <dataisbeautiful@users.noreply.github.com>
2016-01-21 20:23:11 +0800

    Update start-master.sh

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker pull epahomov/docker-spark
-docker run -d -t -P --name spark_master epahomov/docker-spark /start-master.sh "$@"
+docker build -t docker-spark .
+docker run -d -t -P --name spark_master docker-spark /start-master.sh "$@"
