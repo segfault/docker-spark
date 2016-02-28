7af2c17d25748d24a765cc38f9006d573195aec6
Mark Guzman <segfault@hasno.info>
2016-02-27 23:43:29 -0500

    grabbing the last line of the hosts file instead of the first.

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull segy/docker-spark
-docker run -d -t -P --name spark_master segy:docker-spark /start-master.sh "$@"
+docker run -d -t -P --name spark_master segy/docker-spark /start-master.sh "$@"
