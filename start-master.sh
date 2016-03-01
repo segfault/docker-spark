f0cb5ca9a0369cd13ba7e0c835c6b4cb4bd65411
Mark Guzman <segfault@hasno.info>
2016-03-01 00:04:57 -0500

    switching from legacy network links to net aliases docker networks provide their own dns service which should allow for cross-host access

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull segy/docker-spark
-docker run -d -t -P -p 7077:7077 -p 8085:8080 --name spark_master --net spark-net segy/docker-spark /start-master.sh "$@"
+docker run -d -t -P -p 7077:7077 -p 8085:8080 --name spark_master --net spark-net --net-alias spark_master segy/docker-spark /start-master.sh "$@"
