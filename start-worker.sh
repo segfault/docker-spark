12482bdd398cbf2b056161fae0676d0b85514012
Mark Guzman <segfault@hasno.info>
2016-02-29 22:12:50 -0500

    using a multi-host network named spark-net

--- start-worker.sh
+++ start-worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull segy/docker-spark
-docker run -d -t -P --link spark_master:spark_master segy/docker-spark /start-worker.sh "$@"
+docker run -d -t -P --link spark_master:spark_master --net spark-net segy/docker-spark /start-worker.sh "$@"
