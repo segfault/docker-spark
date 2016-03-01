80bf539b3c01027e64dbc0ec34624d423b439b84
Mark Guzman <segfault@hasno.info>
2016-03-01 00:16:58 -0500

    keeping some link support around but defaulting to dns

--- scripts/spark-shell.sh
+++ scripts/spark-shell.sh
@@ -3,7 +3,7 @@ export SPARK_LOCAL_IP=`awk 'END {print $1}' /etc/hosts`
 /remove_alias.sh # problems with hostname alias, see https://issues.apache.org/jira/browse/SPARK-6680
 cd /usr/local/spark
 ./bin/spark-shell \
-	--master spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_ENV_SPARK_MASTER_PORT}  \
+	--master spark://${SPARK_MASTER_PORT_7077_TCP_ADDR:-spark_master}:${SPARK_MASTER_ENV_SPARK_MASTER_PORT:-7077}  \
 	-i ${SPARK_LOCAL_IP} \
 	--properties-file /spark-defaults.conf \
 	"$@" 
--- spark-shell.sh
+++ spark-shell.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull segy/docker-spark
-docker run -i -t -P --link spark_master:spark_master --net spark-net segy/docker-spark /spark-shell.sh "$@"
+docker run -i -t -P --net spark-net segy/docker-spark /spark-shell.sh "$@"
