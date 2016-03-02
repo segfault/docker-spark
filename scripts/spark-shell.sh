2a43f137d0ed4788a872e70b40294540f453201a
Mark Guzman <segfault@hasno.info>
2016-03-01 23:46:11 -0500

    more docker networking changes extract the network ip from hostname so the communication path works

--- scripts/spark-shell.sh
+++ scripts/spark-shell.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SPARK_MASTER_PORT_7077_TCP_ADDR=`getent hosts spark_master | awk '{ print $1 }'`
-export SPARK_LOCAL_IP=`awk 'END {print $1}' /etc/hosts`
+export SPARK_LOCAL_IP=`hostname -I | awk '{print $1}'`
 /remove_alias.sh # problems with hostname alias, see https://issues.apache.org/jira/browse/SPARK-6680
 cd /usr/local/spark
 ./bin/spark-shell \
